bc185d4e2e5e935c85549478d63dc8e6778b9cd1
lsd198 <dhailal@tcd.ie>
2020-11-25 00:17:13 +0000

    Saving for 25/112/2011 Check on line 63

--- FGorwardProp.py
+++ FGorwardProp.py
@@ -7,16 +7,19 @@ from math import exp
 
 def initialize_network(data, n_out, n_hidden, n_neuron, split):
     len_row_data = len(data)
-    wt_hl = [[[random() for i in range(len(data.columns))] for j in range(n_neuron)] for k in range(n_hidden)]
-    # wt_hl = [[random() for i in range(n_neuron + 1)] for i in range(n_hidden)]
+    wt_hl = [[[random() for i in range(len(data.columns) - 1)] for j in range(n_neuron)] for k in range(n_hidden)]
     wt_ne = [[random() for i in range(n_neuron)] for i in range(n_out)]
-    data.insert(max(data.columns.unique()), max(data.columns.unique()) + 1, [1] * 210)
-    target_data = pd.Series.to_list((data[len(data.columns) - 2]))
-    data.drop([len(data.columns) - 2], axis='columns', inplace=True)
-    dataset = data.copy()
-    ttdata = split_traintest(dataset, split)
-    sumprod(ttdata[0], wt_hl, n_out, n_hidden, n_neuron)
+    target_data = data[len(data.columns)-1]
+    data.drop([len(data.columns) - 1], axis='columns', inplace=True)
+    ttdata = split_traintest(data, split)
+    sumprod(nor_data(ttdata[0]), wt_hl, wt_ne, n_out, n_hidden, n_neuron)
 
+
+def nor_data(dataset):
+    newdata=pd.DataFrame()
+    for row in dataset:
+        newdata.insert(row, row, (dataset[row] - dataset[row].max())/ (dataset[row].max() - dataset[row].min()))
+    return newdata
 
 def split_traintest(dataset, split):
     a = 0
@@ -35,27 +38,43 @@ def split_traintest(dataset, split):
     return split_data
 
 
-def sumprod(tr_data, wt_hl, n_out, n_hidden, n_neuron):
+def sumprod(tr_data, wt_hl, wt_ne, n_out, n_hidden, n_neuron):
     error = []
     sum_act = []
-    falg=0
+    flag = 0
     for i in range(len(tr_data)):
         ar1 = np.array(list(tr_data.iloc[i]))
-        for l in range(n_hidden):
-            for k in range(n_neuron):
-                ar2 = np.array(wt_hl[l][k])
-                arr3 = ar1 * ar2
-                sum_act.append(sum(arr3))
-            ar1=np.array(sum_act)
+        for l in range(n_hidden + 1):
+            if flag <= (n_hidden - 1):
+                sum_act.clear()
+                for k in range(n_neuron):
+                    ar2 = np.array(wt_hl[l][k])
+                    arr3 = ar1 * ar2
+                    sum_act.append(sum(arr3))
+                ar1 = np.array(sum_act)
+                flag = flag + 1
+            else:
+                sum_act.clear()
+                for m in range(len(wt_ne)):
+                    ar2 = np.array(wt_ne[i])
+                    arr3 = sum(ar1 * ar2)
+                    arr4 = 1 / (1 + exp(-arr3))
+                    sum_act.append(arr3)
+                errorcal(sum_act)
 
 
+def sep_target(ttdata):
+    tg=[]
+    for val in ttdata:
+        for row in val:
+            if row == len(val.columns)-1:
+                tg.append(val[row])
+    return tg
+def errorcal(arr4):
+    print('')
 
-def activation():
-    print('adsf')
-
-
-def forward_prop():
-    print('dasf')
+def remove_target(ttdata):
+    print('')
 
 
 def backward_propagation():
@@ -75,7 +94,7 @@ def fileupload(filename):
     return dataset
 
 
-seed(2)
+seed(121)
 data = fileupload('datafile.csv')
 data = (data.iloc[np.random.permutation(len(data))]).reset_index(drop=True)
 initialize_network(data, n_out=len(data[len(data.columns) - 1].unique()), n_hidden=5, n_neuron=7, split=3)
