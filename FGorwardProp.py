78e29e92aa7cc175e61a9fd2e8fd2098db27c0ee
lsd198 <dhailal@tcd.ie>
2020-11-25 10:49:42 +0000

    added bias term sum

--- FGorwardProp.py
+++ FGorwardProp.py
@@ -7,10 +7,11 @@ from math import exp
 
 def initialize_network(data, n_out, n_hidden, n_neuron, split):
     len_row_data = len(data)
-    wt_hl = [[[random() for i in range(len(data.columns) - 1)] for j in range(n_neuron)] for k in range(n_hidden)]
+    wt_hl = [[[random() for i in range(len(data.columns))] for j in range(n_neuron)] for k in range(n_hidden)]
     wt_ne = [[random() for i in range(n_neuron)] for i in range(n_out)]
     target_data = data[len(data.columns)-1]
     data.drop([len(data.columns) - 1], axis='columns', inplace=True)
+    data.insert(len(data.columns), len(data.columns), [1] * 210)
     ttdata = split_traintest(data, split)
     sumprod(nor_data(ttdata[0]), wt_hl, wt_ne, n_out, n_hidden, n_neuron)
 
@@ -18,7 +19,7 @@ def initialize_network(data, n_out, n_hidden, n_neuron, split):
 def nor_data(dataset):
     newdata=pd.DataFrame()
     for row in dataset:
-        newdata.insert(row, row, (dataset[row] - dataset[row].max())/ (dataset[row].max() - dataset[row].min()))
+        newdata.insert(row, row, (dataset[row] - dataset[row].min())/ (dataset[row].max() - dataset[row].min()))
     return newdata
 
 def split_traintest(dataset, split):
@@ -47,7 +48,7 @@ def sumprod(tr_data, wt_hl, wt_ne, n_out, n_hidden, n_neuron):
         for l in range(n_hidden + 1):
             if flag <= (n_hidden - 1):
                 sum_act.clear()
-                for k in range(n_neuron):
+                for k in range(n_neuron+1):
                     ar2 = np.array(wt_hl[l][k])
                     arr3 = ar1 * ar2
                     sum_act.append(sum(arr3))
@@ -56,9 +57,9 @@ def sumprod(tr_data, wt_hl, wt_ne, n_out, n_hidden, n_neuron):
             else:
                 sum_act.clear()
                 for m in range(len(wt_ne)):
-                    ar2 = np.array(wt_ne[i])
+                    ar2 = np.array(wt_ne[m])
                     arr3 = sum(ar1 * ar2)
-                    arr4 = 1 / (1 + exp(-arr3))
+                    arr4 = 1 / (1 + np.exp(-arr3))
                     sum_act.append(arr3)
                 errorcal(sum_act)
 
