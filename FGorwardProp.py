ad88c01be0e5e7bfb83ea65e10c36c416a2c2106
lsd198 <dhailal@tcd.ie>
2020-11-26 20:57:49 +0000

    update 20:57 26/11/2020

--- FGorwardProp.py
+++ FGorwardProp.py
@@ -7,7 +7,6 @@ from math import exp
 
 def initialize_network(data, n_out, n_hidden, n_neuron, split):
     len_row_data = len(data)
-    np.random.seed(121)
     wt_hl = [[[random() for i in range(len(data.columns))] for j in range(n_neuron)] for k in range(n_hidden)]
     wt_ne = [[random() for i in range(n_neuron + 1)] for i in range(n_out)]
     target_data = split_traintest(data[len(data.columns) - 1], split)
@@ -15,7 +14,7 @@ def initialize_network(data, n_out, n_hidden, n_neuron, split):
     error_val.sort()
     data.drop([len(data.columns) - 1], axis='columns', inplace=True)
     ttdata = split_traintest(nor_data(data), split)
-    sumprod(ttdata[0], wt_hl, wt_ne, n_out, n_hidden, n_neuron, error_val,target_data[0])
+    return len_row_data, wt_hl, wt_ne, target_data, error_val, ttdata
 
 
 def nor_data(dataset):
@@ -42,9 +41,11 @@ def split_traintest(dataset, split):
     return split_data
 
 
-def sumprod(tr_data, wt_hl, wt_ne, n_out, n_hidden, n_neuron, error_val,target_data):
+def sumprod(tr_data, wt_hl, wt_ne, n_hidden, n_neuron, error_val, target_data):
     # error = pd.DataFrame(,)
-    err_final=[]
+    output_all_layer=[]
+    err_op_neuron = []
+    err_total = []
     sum_act = []
     flag = 0
     # Belo loop will run for total len of dataset
@@ -58,19 +59,21 @@ def sumprod(tr_data, wt_hl, wt_ne, n_out, n_hidden, n_neuron, error_val,target_d
                 for k in range(n_neuron):
                     ar2 = np.array(wt_hl[l][k])
                     arr3 = ar1 * ar2[0:len(ar2) - 1]
+                    output_all_layer.append(arr3)
                     sum_act.append(sum(arr3) + ar2[-1])
                 ar1 = np.array(sum_act)
                 flag = flag + 1
             else:
                 # This else part will run only for the final layer that we have  in the network
                 sum_act.clear()
-                output = []
+                output_final_layer = []
                 for m in range(len(wt_ne)):
                     ar2 = np.array(wt_ne[m])
                     arr3 = sum(ar1 * ar2[len(ar2) - 1]) + ar2[-1]
-                    output.append(1 / (1 + exp(-arr3)))
+                    output_final_layer.append(1 / (1 + exp(-arr3)))
                 flag = 0
-                errorcal(i, output, error_val,target_data,err_final)
+                errorcal(i, output_final_layer, error_val, target_data, err_total, err_op_neuron)
+    return output_final_layer,output_all_layer
 
 
 def sep_target(ttdata):
@@ -82,24 +85,18 @@ def sep_target(ttdata):
     return tg
 
 
-def errorcal(i, output, error_val,target_data,err_final):
-    e_list=[]
-    diff_list=[]
+def errorcal(i, output, error_val, target_data, err_total, err_op_neuron):
+    e_list = []
+    diff_list = []
     for val in error_val:
         if val == target_data[i]:
             e_list.append(1)
         else:
             e_list.append(0)
-    for l1,l2 in zip(e_list,output):
-        diff_list.append((l1-l2)*(l1-l2))
-    err_final.append(sum(diff_list))
-
-
-
-
-
-
-
+    for l1, l2 in zip(e_list, output):
+        diff_list.append((l1 - l2) * (l1 - l2))
+    err_op_neuron.append(diff_list)
+    err_total.append(sum(diff_list))
 
 
 def remove_target(ttdata):
@@ -118,14 +115,27 @@ def wt_update():
     print('asdf')
 
 
+def epoch(data, out, hidden, neuron, split, epochs):
+    print('inside the function of epoch')
+    init_op = initialize_network(data, out, hidden, neuron, split)
+    print('dasf')
+    for i in range(epochs):
+        for_op=sumprod(init_op[5][0], init_op[1], init_op[2], hidden, neuron, init_op[4], init_op[3][0])
+        backward_propagation(for_op[0])
+    # return len_row_data, wt_hl, wt_ne, target_data, error_val, ttdata
+    # return len_row_data, wt_hl, wt_ne, target_data, error_val, ttdata
+    # for
+    # sumprod(ttdata[0], wt_hl, wt_ne, n_out, n_hidden, n_neuron, error_val, target_data[0])
+
+
 def fileupload(filename):
     dataset = pd.read_csv(filename, header=None)
     return dataset
 
 
-
+seed(1)
 data = fileupload('datafile.csv')
 data = (data.iloc[np.random.permutation(len(data))]).reset_index(drop=True)
-initialize_network(data, n_out=len(data[len(data.columns) - 1].unique()), n_hidden=5, n_neuron=7, split=3)
+epoch(data, len(data[len(data.columns) - 1].unique()), 5, 7, 3, 500)
 print('asdfasd')
 # below function will initilaize the initial weights for both the hidden neurons  and output layer neurons
